5580cccbb090924c0930e322990170f439f664db
Maral Erfanian <me.maralerfanian@gmail.com>
2023-01-26 21:48:07 +0100

    write routers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import App from './App';
-import theme from './theme';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./App";
+import theme from "./theme";
+import Admin from "./screens/admin-dashboard/Dashboard";
+import Products from "./screens/products/Products";
+import Checkout from "./screens/checkout/Checkout";
+import Signin from "./screens/sign-in/SignIn";
 
-const rootElement = document.getElementById('root');
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
-  </ThemeProvider>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
+  </ThemeProvider>
 );
